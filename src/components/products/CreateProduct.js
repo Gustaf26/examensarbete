cc002c85a7ec20c14b70aa3f2a86ede91ec31c56
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-12-19 16:33:08 +0100

    going back to singleProduct instead of category after loading - works fine && spinner attached while loading data

--- src/components/products/CreateProduct.js
+++ src/components/products/CreateProduct.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import UploadImageDropzone from "./UploadImageDropzone";
 import { Row, Col, Card, Form, Button, Alert } from "react-bootstrap";
+import { BounceLoader } from "react-spinners";
 import { useNavigate } from "react-router-dom";
 import { db } from "../../firebase";
 import { useCreate } from "../../contexts/CreateContext";
@@ -56,14 +57,19 @@ const CreateProduct = () => {
         id: ranNumber,
       });
 
-      setTimeout(() => {
+      const timingFunction = setInterval(() => {
+        setLoading(true);
         db.collection(`${productOption}`)
           .doc(`${ranNumber}`)
           .get()
           .then((doc) => {
-            setSingleProduct(doc.data());
-            navigate(`/products/${productOption}`);
-          }, 1000);
+            if (doc.data()) {
+              setSingleProduct(doc.data());
+              navigate(`/products/${productOption}/${ranNumber}`);
+              setLoading(false);
+              clearInterval(timingFunction);
+            }
+          }, 2000);
       });
     } catch (e) {
       setError(e.message);
@@ -75,79 +81,88 @@ const CreateProduct = () => {
     <>
       <Row>
         <Col md={{ span: 6, offset: 3 }}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Create a product entry</Card.Title>
+          {!loading && (
+            <Card>
+              <Card.Body>
+                <Card.Title>Create a product entry</Card.Title>
 
-              {error && <Alert variant="danger">{error}</Alert>}
+                {error && <Alert variant="danger">{error}</Alert>}
 
-              <Form onSubmit={handleSubmit}>
-                <Form.Group id="title">
-                  <Form.Label>Product name</Form.Label>
-                  <Form.Control
-                    type="title"
-                    onChange={handleNameChange}
-                    value={name}
-                    required
-                  />
-                  {name && name.length < 4 && (
-                    <Form.Text className="text-danger">
-                      Please enter a name at least 4 characters long.
-                    </Form.Text>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group id="title">
+                    <Form.Label>Product name</Form.Label>
+                    <Form.Control
+                      type="title"
+                      onChange={handleNameChange}
+                      value={name}
+                      required
+                    />
+                    {name && name.length < 4 && (
+                      <Form.Text className="text-danger">
+                        Please enter a name at least 4 characters long.
+                      </Form.Text>
+                    )}
+                  </Form.Group>
+                  <Form.Group id="description">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                      type="title"
+                      onChange={handleDescriptionChange}
+                      value={description}
+                      required
+                    />
+                    {description && description.length < 20 && (
+                      <Form.Text className="text-danger">
+                        Please enter a description at least 20 characters long.
+                      </Form.Text>
+                    )}
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Choose product category</Form.Label>
+                    <Form.Control
+                      as="select"
+                      required
+                      id="inlineFormCustomSelect"
+                      custom
+                      onClick={(e) =>
+                        setProductOption(e.target.value.toLowerCase())
+                      }
+                    >
+                      {productCategories &&
+                        productCategories.map((category) => (
+                          <option>{category.name.toUpperCase()}</option>
+                        ))}
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group id="price">
+                    <Form.Label>Price</Form.Label>
+                    <Form.Control
+                      type="title"
+                      onChange={handlePrice}
+                      value={prodPrice}
+                      required
+                    />
+                    {prodPrice && prodPrice === "0" && (
+                      <Form.Text className="text-danger">
+                        Please set the product price.
+                      </Form.Text>
+                    )}
+                  </Form.Group>
+                  {productOption && (
+                    <UploadImageDropzone type={productOption} />
                   )}
-                </Form.Group>
-                <Form.Group id="description">
-                  <Form.Label>Description</Form.Label>
-                  <Form.Control
-                    type="title"
-                    onChange={handleDescriptionChange}
-                    value={description}
-                    required
-                  />
-                  {description && description.length < 20 && (
-                    <Form.Text className="text-danger">
-                      Please enter a description at least 20 characters long.
-                    </Form.Text>
-                  )}
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Choose product category</Form.Label>
-                  <Form.Control
-                    as="select"
-                    required
-                    id="inlineFormCustomSelect"
-                    custom
-                    onClick={(e) =>
-                      setProductOption(e.target.value.toLowerCase())
-                    }
-                  >
-                    {productCategories &&
-                      productCategories.map((category) => (
-                        <option>{category.name.toUpperCase()}</option>
-                      ))}
-                  </Form.Control>
-                </Form.Group>
-                <Form.Group id="price">
-                  <Form.Label>Price</Form.Label>
-                  <Form.Control
-                    type="title"
-                    onChange={handlePrice}
-                    value={prodPrice}
-                    required
-                  />
-                  {prodPrice && prodPrice === "0" && (
-                    <Form.Text className="text-danger">
-                      Please set the product price.
-                    </Form.Text>
-                  )}
-                </Form.Group>
-                {productOption && <UploadImageDropzone type={productOption} />}
-                <Button disabled={loading} type="submit" className="mx-auto">
-                  Create
-                </Button>
-              </Form>
-            </Card.Body>
-          </Card>
+                  <Button disabled={loading} type="submit" className="mx-auto">
+                    Create
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          )}
+          {loading && (
+            <div className="d-flex justify-content-center my-5">
+              <BounceLoader color={"#888"} size={100} />
+            </div>
+          )}
         </Col>
       </Row>
     </>
