e06d25142fe30df78cd74cf038be093d96aed41e
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2021-05-11 23:04:30 +0200

    setting initial password in context on signup too, so I can get hold of it in update profile

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,77 +1,86 @@
-import React, { useRef, useState } from 'react'
-import { Row, Col, Form, Button, Card, Alert } from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
+import React, { useRef, useState } from "react";
+import { Row, Col, Form, Button, Card, Alert } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { useCreate } from "../contexts/CreateContext";
 
 const Signup = () => {
-	const emailRef = useRef()
-	const passwordRef = useRef()
-	const passwordConfirmRef = useRef()
-	const [error, setError] = useState(null)
-	const [loading, setLoading] = useState(false)
-	const { signup } = useAuth()
-	const navigate = useNavigate()
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { signup } = useAuth();
+  const navigate = useNavigate();
+  const { setCurrentPassword } = useCreate();
 
-	const handleSubmit = async (e) => {
-		e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		// make sure user has entered the same password in both input fields
-		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-			return setError("The passwords does not match")
-		}
+    // make sure user has entered the same password in both input fields
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      setError("The passwords does not match");
+      return;
+    }
 
-		setError(null);
+    setCurrentPassword(passwordRef.current.value);
 
-		try {
-			// try to sign up the user with the specified credentials
-			setLoading(true)
-			await signup(emailRef.current.value, passwordRef.current.value)
-			navigate('/')
-		} catch (e) {
-			setError(e.message)
-			setLoading(false)
-		}
-	}
+    setError(null);
 
-	return (
-		<>
-			<Row>
-				<Col md={{ span: 6, offset: 3 }}>
-					<Card>
-						<Card.Body>
-							<Card.Title>Sign Up</Card.Title>
+    try {
+      // try to sign up the user with the specified credentials
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      navigate("/");
+    } catch (e) {
+      setError(e.message);
+      setLoading(false);
+    }
+  };
 
-							{error && (<Alert variant="danger">{error}</Alert>)}
+  return (
+    <>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Sign Up</Card.Title>
 
-							<Form onSubmit={handleSubmit}>
+              {error && <Alert variant="danger">{error}</Alert>}
 
-								<Form.Group id="email">
-									<Form.Label>Email</Form.Label>
-									<Form.Control type="email" ref={emailRef} required />
-								</Form.Group>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
 
-								<Form.Group id="password">
-									<Form.Label>Password</Form.Label>
-									<Form.Control type="password" ref={passwordRef} required />
-								</Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
 
-								<Form.Group id="password-confirm">
-									<Form.Label>Password Confirmation</Form.Label>
-									<Form.Control type="password" ref={passwordConfirmRef} required />
-								</Form.Group>
+                <Form.Group id="password-confirm">
+                  <Form.Label>Password Confirmation</Form.Label>
+                  <Form.Control
+                    type="password"
+                    ref={passwordConfirmRef}
+                    required
+                  />
+                </Form.Group>
 
-								<Button disabled={loading} type="submit">Create Account</Button>
+                <Button disabled={loading} type="submit">
+                  Create Account
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <div className="text-center mt-2">
+            Already have an account? <Link to="/login">Log In</Link>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-							</Form>
-						</Card.Body>
-					</Card>
-					<div className="text-center mt-2">
-						Already have an account? <Link to="/login">Log In</Link>
-					</div>
-				</Col>
-			</Row>
-		</>
-	)
-}
-
-export default Signup
+export default Signup;
