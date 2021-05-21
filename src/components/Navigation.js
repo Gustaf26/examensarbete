d868e6b708e8a6fb1a7a3e2ea0dd296eb00f0819
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2021-05-21 21:01:50 +0200

    styling search input for products

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -56,6 +56,8 @@ const Navigation = () => {
         setCustMenu(true);
       }
     });
+    let inputEl = document.getElementById("product-search");
+    inputEl.focus();
   });
 
   return (
@@ -83,7 +85,7 @@ const Navigation = () => {
                     onChange={changeString}
                     type="text"
                     id="product-search"
-                    placeholder="     Search product"
+                    placeholder="Search product"
                     className="mr-sm-1 ml-5 navitem"
                   />
                   <SearchIcon id="search-icon" />
