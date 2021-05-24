5e33aab14788d19422dffccab684c3c13f809596
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2021-05-25 00:10:29 +0200

    hamburger menu showing right dropdown and menu choices working - login - register - logout

--- src/components/SimpleMenu.js
+++ src/components/SimpleMenu.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import MenuIcon from "@material-ui/icons/Menu";
 
 export default function SimpleMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const { admin, currentUser } = useAuth();
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -32,9 +34,21 @@ export default function SimpleMenu() {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        {!admin && !currentUser && (
+          <div>
+            <MenuItem onClick={handleClose}>
+              <Link to="/login">Login</Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link to="/signup">Register</Link>
+            </MenuItem>
+          </div>
+        )}
+        {(admin || currentUser) && (
+          <MenuItem onClick={handleClose}>
+            <Link to="logout">Logout</Link>
+          </MenuItem>
+        )}
       </Menu>
     </div>
   );
