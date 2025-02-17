035302ea357ec82e8d3c390ebc5e1824f7906e5c
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2025-02-17 23:07:05 +0100

    Some minor changes

--- src/components/BreadCrumbContainer.js
+++ src/components/BreadCrumbContainer.js
@@ -1,76 +1,89 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 import { Breadcrumb } from "react-bootstrap";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import { useCreate } from "../contexts/CreateContext";
 import { useAuth } from "../contexts/AuthContext";
-import { useMobile } from '../contexts/MobileContext';
+import { useMobile } from "../contexts/MobileContext";
 
-
-import ArrowBack from '@mui/icons-material/ArrowBack';
+import ArrowBack from "@mui/icons-material/ArrowBack";
 
 export default function BreadCrumbContainer({ qty }) {
+	const { productCategories, allProducts } = useCreate();
+	const { admin } = useAuth();
+	const { mobile } = useMobile();
 
-    const { productCategories, allProducts } = useCreate();
-    const { admin } = useAuth()
-    const { mobile } = useMobile()
+	const [breadcrumbs, setBreadcrumbs] = useState([]);
+	const location = useLocation();
+	const navigate = useNavigate();
 
-    const [breadcrumbs, setBreadcrumbs] = useState([])
-    const location = useLocation()
-    const navigate = useNavigate()
+	const dummyProdCategories = productCategories.map((cat) => cat.name);
 
-    const dummyProdCategories = productCategories.map(cat => cat.name)
+	useEffect(() => {
+		if (location.pathname.lastIndexOf("/") === location.pathname.length - 1) {
+			location.pathname = location.pathname.slice(0, location.pathname.length - 1);
+		}
+		let lastDash = location.pathname.lastIndexOf("/");
+		let lastChar = location.pathname.length;
+		let subcategory = location.pathname.slice(lastDash + 1, lastChar);
+		let overcategory = location.pathname.replace(`/${subcategory}`, "");
+		overcategory = overcategory.slice(
+			overcategory.lastIndexOf("/") + 1,
+			overcategory.length
+		);
 
+		let dummyBread = ["Home"];
+		if (overcategory !== "products") dummyBread.push(overcategory);
+		else {
+			overcategory = "";
+		}
 
-    useEffect(() => {
+		let singleProd;
 
-        if (location.pathname.lastIndexOf('/') === location.pathname.length - 1) {
-            location.pathname = location.pathname.slice(0, location.pathname.length - 1)
-        }
-        let lastDash = location.pathname.lastIndexOf('/');
-        let lastChar = location.pathname.length
-        let subcategory = location.pathname.slice(lastDash + 1, lastChar);
-        let overcategory = location.pathname.replace(`/${subcategory}`, '')
-        overcategory = overcategory.slice(overcategory.lastIndexOf('/') + 1, overcategory.length)
+		if (subcategory && Number(subcategory)) {
+			singleProd = allProducts.filter((prod) => prod.id === Number(subcategory));
+		}
+		if (singleProd && singleProd.length > 0) {
+			subcategory = singleProd[0].name;
+		}
+		if (subcategory) {
+			dummyBread.push(subcategory);
+		}
 
-        let dummyBread = ['Home']
-        if (overcategory !== 'products') dummyBread.push(overcategory)
-        else { overcategory = "" }
+		if (!overcategory && !subcategory) {
+			dummyBread = [];
+		}
 
-        let singleProd;
+		console.log(dummyBread);
+		setBreadcrumbs(dummyBread);
+	}, [location]);
 
-        if (subcategory && Number(subcategory)) {
-            singleProd = allProducts.filter(prod => prod.id === Number(subcategory))
-        }
-        if (singleProd && singleProd.length > 0) {
-            subcategory = singleProd[0].name
-        }
-        if (subcategory) {
-            dummyBread.push(subcategory)
-        }
-
-        if (!overcategory && !subcategory) { dummyBread = [] }
-
-        console.log(dummyBread)
-        setBreadcrumbs(dummyBread)
-
-    }, [location])
-
-    return (<Breadcrumb style={!(mobile && admin) ? { margin: '20px' } : {}}>
-        <ArrowBack style={{ color: '#0d6efd' }} sx={{ mr: 1, ml: 1, mt: 0.4 }} fontSize="medium" />
-        {breadcrumbs && breadcrumbs.map((bread, i) => {
-
-            return bread === 'Home' ? (
-                <Breadcrumb.Item>
-                    <Link to={admin ? "/cms/index" : "/"}>Home</Link>
-                </Breadcrumb.Item>) :
-                dummyProdCategories.includes(bread.toLowerCase()) ? (<Breadcrumb.Item>
-                    <Link to={admin ? `/cms/products/${bread}` : `/products/${bread}`}>{bread}</Link>
-                </Breadcrumb.Item>) :
-                    (<Breadcrumb.Item>
-                        {bread}
-                    </Breadcrumb.Item>)
-        })}
-    </Breadcrumb>)
+	return (
+		<Breadcrumb
+			style={!(mobile && admin) ? { margin: "40px" } : { margin: "50px" }}
+		>
+			<ArrowBack
+				style={{ color: "#0d6efd" }}
+				sx={{ mr: 1, ml: 1, mt: 0.4 }}
+				fontSize='medium'
+			/>
+			{breadcrumbs &&
+				breadcrumbs.map((bread, i) => {
+					return bread === "Home" ? (
+						<Breadcrumb.Item>
+							<Link to={admin ? "/cms/index" : "/"}>Home</Link>
+						</Breadcrumb.Item>
+					) : dummyProdCategories.includes(bread.toLowerCase()) ? (
+						<Breadcrumb.Item>
+							<Link to={admin ? `/cms/products/${bread}` : `/products/${bread}`}>
+								{bread}
+							</Link>
+						</Breadcrumb.Item>
+					) : (
+						<Breadcrumb.Item>{bread}</Breadcrumb.Item>
+					);
+				})}
+		</Breadcrumb>
+	);
 }
--- src/components/products/ProductsGrid.js
+++ src/components/products/ProductsGrid.js
@@ -3,78 +3,124 @@ import React from "react";
 // import { Link } from "react-router-dom";
 
 import { Row } from "react-bootstrap";
-import Icon from '@mui/material/Icon';
+import Icon from "@mui/material/Icon";
 
 import { BounceLoader } from "react-spinners";
-
 
 import { useAuth } from "../../contexts/AuthContext";
 import { useCreate } from "../../contexts/CreateContext";
 import { useMobile } from "../../contexts/MobileContext";
 
-import MobileList from '../../cms_components/MobileList'
-import useMobileStyles from '../../hooks/useMobileStyles'
+import MobileList from "../../cms_components/MobileList";
+import useMobileStyles from "../../hooks/useMobileStyles";
 
-import Navigation from '../Navigation'
-import ProductCard from '../products/ProductCard'
-import CardContainer from '../products/CardContainer'
+import Navigation from "../Navigation";
+import ProductCard from "../products/ProductCard";
+import CardContainer from "../products/CardContainer";
 import BreadcrumbContainer from "../BreadCrumbContainer";
 
 const ProductsGrid = ({ products, type }) => {
+	const [loading, setLoading] = React.useState(true);
+	// const navigate = useNavigate();
+	const { admin } = useAuth();
+	const { setProductOption } = useCreate();
+	const { mobile, mobileDisplays, setMobileDisplays } = useMobile();
 
-  const [loading, setLoading] = React.useState(true)
-  // const navigate = useNavigate();
-  const { admin } = useAuth();
-  const { setProductOption } = useCreate();
-  const { mobile, mobileDisplays, setMobileDisplays } = useMobile()
+	const { containerStyles, microMobile } = useMobileStyles();
 
-  const { containerStyles, microMobile } = useMobileStyles()
+	return (
+		<>
+			{loading && (
+				<div
+					style={{ marginTop: "10%" }}
+					className='d-flex justify-content-center align-items-center'
+				>
+					<BounceLoader color={"#888"} size={100} />
+				</div>
+			)}
+			<div
+				id='dummy-container-products'
+				style={
+					admin
+						? {
+								position: "absolute",
+								top: mobile ? "60px" : "226px",
+								left: mobile ? "40px" : "240px",
+								width: mobile ? "calc(100% - 40px)" : "calc(100% - 240px)",
+								paddingRight: !mobile && admin ? "200px" : "",
+						  }
+						: {}
+				}
+				onClick={(e) => {
+					if (e.target.id === "dummy-container-products") setMobileDisplays(false);
+				}}
+			>
+				{!(admin && mobile) && <BreadcrumbContainer />}
 
+				<Row
+					id='dummy-container-mobile'
+					onLoad={(e) => {
+						setProductOption(type);
+					}}
+					style={
+						mobile && admin
+							? { ...containerStyles, padding: "10px 10px" }
+							: mobile
+							? {
+									marginTop: "5rem",
+									padding: "20px",
+									display: "flex",
+									justifyContent: "center",
+							  }
+							: admin
+							? { margin: "3rem auto", justifyContent: "center" }
+							: { margin: "3rem auto" }
+					}
+				>
+					{admin && mobile && <Navigation />}
 
+					{mobile && admin && !microMobile && (
+						<Icon
+							className='icon-mobile-displays'
+							onClick={() => setMobileDisplays(!mobileDisplays)}
+							style={{
+								border: "1px solid lightgrey",
+								width: "40px",
+								height: "40px",
+								textAlign: "left",
+								zIndex: "5",
+								margin: "0 auto",
+								padding: "8px",
+								borderRadius: "5px",
+								position: "absolute",
+								top: `-20px`,
+								left: "45%",
+								backgroundColor: "rgb(255, 255, 255)",
+							}}
+							color='primary'
+						>
+							device_unknown
+						</Icon>
+					)}
 
-  return (<>
-    {loading && (
-      <div style={{ marginTop: '10%' }} className="d-flex justify-content-center align-items-center">
-        <BounceLoader color={"#888"} size={100} />
-      </div>
-    )}
-    <div id="dummy-container-products"
-      style={admin ? {
-        position: 'absolute', top: mobile ? '60px' : '220px', left: mobile ? '40px' : '240px',
-        width: mobile ? 'calc(100% - 40px)' : 'calc(100% - 240px)', paddingRight: !mobile && admin ? '200px' : ''
-      } : {}} onClick={(e) => { if (e.target.id === "dummy-container-products") setMobileDisplays(false) }}>
-      {!(admin && mobile) && <BreadcrumbContainer />}
+					{mobileDisplays && <MobileList />}
 
-      <Row id="dummy-container-mobile" onLoad={(e) => { setProductOption(type) }}
-        style={mobile && admin ? { ...containerStyles, padding: '10px 10px' } : mobile ? {
-          marginTop: '5rem', padding: '20px',
-          display: 'flex', justifyContent: 'center'
-        } :
-          admin ? { margin: '-3rem auto', justifyContent: 'center' } : {}}>
-
-        {admin && mobile && <Navigation />}
-
-        {mobile && admin && !microMobile && <Icon className="icon-mobile-displays" onClick={() => setMobileDisplays(!mobileDisplays)} style={{
-          border: '1px solid lightgrey',
-          width: '40px', height: '40px', textAlign: 'left', zIndex: '5', margin: '0 auto', padding: '8px',
-          borderRadius: '5px', position: 'absolute', top: `-20px`, left: '45%', backgroundColor: 'rgb(255, 255, 255)'
-        }}
-          color='primary'>device_unknown</Icon>}
-
-        {mobileDisplays && <MobileList />}
-
-        <CardContainer onLoad={(e) => e.target.scrollIntoView({ block: 'start' })}>
-
-          {products &&
-            products.map((item, i) => (
-              <ProductCard setLoading={setLoading} index={i} id={`${item.id}`} key={item.id} item={item} />
-            ))}
-
-        </CardContainer>
-      </Row >
-    </div >
-  </>
-  );
+					<CardContainer onLoad={(e) => e.target.scrollIntoView({ block: "start" })}>
+						{products &&
+							products.map((item, i) => (
+								<ProductCard
+									setLoading={setLoading}
+									index={i}
+									id={`${item.id}`}
+									key={item.id}
+									item={item}
+								/>
+							))}
+					</CardContainer>
+				</Row>
+			</div>
+		</>
+	);
 };
 
 export default ProductsGrid;
