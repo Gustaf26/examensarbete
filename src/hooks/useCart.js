23a336a397eafcec30c20238a915c59e879828ea
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2025-03-04 11:49:03 +0100

    Updating cart and preserving the order of the products when clicking

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -14,13 +14,15 @@ function useCart() {
         if (singleProd.length) productToUpdate = singleProd[0]
         else return
 
-
         if (productToUpdate && action === 'plus') productToUpdate.qty = Number(productToUpdate.qty + 1)
         else if (productToUpdate.qty && action === 'minus' && productToUpdate.qty !== 0) Number(productToUpdate.qty -= 1)
 
-        let otherProds = allProducts.filter(prod => prod.id !== item.id)
+        let allProdsDummy = allProducts.map(prod => {
+            if (prod.id === item.id) prod.qty = productToUpdate.qty
+            return prod
+        })
 
-        setProducts([...otherProds, productToUpdate])
+        setProducts(allProdsDummy)
     }
 
     return updateCart
