9f4b463ef5df275b5830b669b34508aaf475115d
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2025-03-16 02:19:50 +0100

    Working on the desktop home slider

--- src/components/home_sections/DesktopHome.js
+++ src/components/home_sections/DesktopHome.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -8,34 +8,61 @@ import { useCreate } from "../../contexts/CreateContext"
 function DesktopHome() {
 
     const { allProducts } = useCreate()
-    const [slidesShowing, setSlidesShowing] = useState(7)
-    const [slides, setSlides] = useState(allProducts)
+    // const [slidesShowing, setSlidesShowing] = useState(7)
+    const [slides, setSlides] = useState()
+    const slidesref = useRef()
+    const [rightMoves, setRightMoves] = useState(0)
+    const [leftMoves, setLeftMoves] = useState(0)
 
+
+    const moveSlides = (direction) => {
+
+        slidesref.current.style.transform = 'none'
+
+        let previousSlides = [...slides]
+        if (direction === 'right') {
+
+            // Just setting a maximum for the slider to have arrays of products
+            if (slides.length < 10) {
+                previousSlides.push(allProducts)
+            }
+            setRightMoves(prev => prev + 1)
+            setLeftMoves(0)
+        }
+        else {
+            if (rightMoves === 0) return
+            setLeftMoves((prev) => prev + 1)
+            setRightMoves(0)
+            setSlides([allProducts])
+            return
+        }
+        setSlides(previousSlides)
+
+        slidesref.current.style.transition = 'transform 1s linear'
+        slidesref.current.style.transform = direction === 'right' ? `translateX(-${rightMoves * 150}px)` : `translateX(0px)`
+    }
 
     useEffect(() => {
-        if (slidesShowing >= allProducts.length) {
-            setSlides(allProducts);
-            setSlidesShowing(7)
-        }
-        else if (slidesShowing <= 7) {
-            setSlides(allProducts)
-            setSlidesShowing(allProducts.length - 1)
-        }
-        // alert(slidesShowing)
-    }, [slidesShowing, allProducts])
+        setSlides([allProducts])
+    }, [allProducts])
+
 
     return (<div style={{ width: '100%', overflowX: 'hidden' }}>
         <div id="home-card1"
             className='main-cardp-1'
-            style={{ display: 'flex', backgroundColor: 'white', justifyContent: 'space-between', borderRadius: '10px', width: '80%', margin: '5rem auto' }}
+            style={{
+                display: 'flex', justifyContent: 'space-between',
+                borderRadius: '10px', width: '80%', margin: '5rem auto'
+            }}
         >
-            <img alt="home-wellcome-picture"
+            <img alt="home-welcome-picture"
                 className='mx-auto p-0'
                 style={{ margin: '15px' }}
                 variant='top'
                 src='https://cdn.pixabay.com/photo/2017/09/17/19/43/woman-2759503__340.jpg'
             />
-            <div id='home-card-text' style={{ margin: '15px', display: 'flex', flexDirection: 'column', alignItems: 'end' }} className='text-muted mediums px-2'>
+            <div id='home-card-text' style={{ margin: '15px', display: 'flex', flexDirection: 'column', alignItems: 'end' }}
+                className='text-muted mediums px-2'>
                 <h3 style={{ color: 'brown', width: '100%' }}>Cheapest In The Market</h3>
                 <div style={{ margin: '2rem auto', color: 'rgb(109, 44, 38)' }}>
                     <p>Work Hard, Not on Your Wallet. Discover our collection of
@@ -59,29 +86,35 @@ function DesktopHome() {
                     <p>Discover Quality Workwear That Fits Your Budget. Find Your Perfect Work Outfit, Without the Premium Price.</p>
                 </div>
             </div>
-            <div style={{ display: 'flex', margin: '2rem auto', position: 'relative', width: '80%', justifyContent: 'center' }}>
+            <div id="home-slider" style={{ display: 'flex', margin: '2rem auto', position: 'relative', width: '80%', justifyContent: 'center' }}>
                 <div style={{
-                    position: 'absolute', height: '60px', width: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center',
+                    position: 'absolute', zIndex: '10', height: '60px', width: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center',
                     left: '-40px', top: '40%', backgroundColor: 'rgba(216, 158, 0, 1)', padding: '10px', borderRadius: '50px'
                 }}>
                     <ArrowBackIosIcon style={{ color: 'brown', marginLeft: '10px' }}
-                        onClick={() => setSlidesShowing((prev) => prev - 7)}
+                        onClick={() => moveSlides('left')}
                     ></ArrowBackIosIcon>
                 </div>
-                {slides && slides.map((prod, i) => {
-                    if (i <= slidesShowing) {
-                        return (
-                            <img style={{ margin: '0', padding: '0', width: '200px', height: '300px', objectFit: 'cover' }}
-                                alt={prod.description} src={prod.thumbnail} />
-                        )
-                    }
-                    else return null
-                })}
+                <div style={{ width: 'calc(100vw - 20%)' }}>
+                    <div style={{ display: 'flex', flexWrap: 'nowrap' }} ref={slidesref}>
+                        {slides && slides.map((arr, i) => {
+                            return (<div style={{ display: 'inline flex', flexWrap: 'nowrap', width: 'fit-content' }}>
+                                {arr.map(prod => (
+                                    <img style={{ margin: '0', padding: '0', width: '200px', height: '300px', objectFit: 'cover' }}
+                                        alt={prod.description} src={prod.thumbnail} />
+                                ))
+                                }
+                            </div>)
+                        })}
+
+                    </div>
+                </div>
+
                 <div style={{
-                    position: 'absolute', height: '60px', width: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center',
+                    position: 'absolute', zIndex: '10', height: '60px', width: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center',
                     right: '-40px', top: '40%', backgroundColor: 'rgba(216, 158, 0, 1)', padding: '10px', borderRadius: '50px'
                 }}>
-                    <ArrowForwardIosIcon onClick={() => setSlidesShowing((prev) => prev + 7)}
+                    <ArrowForwardIosIcon onClick={() => moveSlides('right')}
                         style={{ color: 'brown', marginRight: '0px' }}></ArrowForwardIosIcon>
                 </div>
 
