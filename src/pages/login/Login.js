6b345b940cdc21ae160dc26657f04461b2a2af58
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2025-05-30 17:33:12 +0200

    Changing the file structure to a typichal intermediate / medum size application

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,8 +2,8 @@
 import React, { useRef, useState } from "react";
 import { Row, Col, Form, Button, Card, Alert } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
-import { useMobile } from "../contexts/MobileContext";
+import { useAuth } from "../../contexts/AuthContext";
+import { useMobile } from "../../contexts/MobileContext";
 
 const Login = () => {
   const emailRef = useRef();
